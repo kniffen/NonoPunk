c7a4ad262955a18f11a88ed64fd24f361604eebc
Kniffen <knifftech@gmail.com>
2018-09-01 08:52:50 +0200

    Moved position

--- src/draw-notification.js
+++ src/draw-notification.js
@@ -15,8 +15,8 @@ export default function drawNotification(app, text) {
   banner.drawRect(-2, 0, banner._width + 4, banner._height)
   banner.endFill()
 
-  banner.y = app.view.height / 2 - banner._height / 2
-  title.y  = app.view.height / 2 - title.height / 2
+  banner.y = 100
+  title.y  = 100 + title.height / 2
   title.x  = app.view.width  / 2 - title.width / 2
 
   app.stage.addChild(banner, title)
