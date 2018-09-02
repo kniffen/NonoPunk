7b55364c1598d4cecf37a831cdee185c6ea7366b
Kniffen <knifftech@gmail.com>
2018-09-02 07:03:32 +0200

    Improved option slider

--- src/draw-options.js
+++ src/draw-options.js
@@ -15,8 +15,39 @@ export default function drawOptions(app, state) {
     buttons: ['back']
   })
 
-  const musicSlider = slider('music volume', 20, 100, sounds.music)
-  const sfxSlider   = slider('sfx volume',   20, musicSlider.y + musicSlider.height + 40, sounds.sfx)
+  const musicSlider = slider(
+    'Music volume', 
+    20, 
+    100, 
+    sounds.music[0].volume, volume => {
+      sounds.music.forEach(sound => {
+        sound.volume = volume
+        sound.muted = volume < 0.01
+      })
+
+      if (volume > 0.01 && !sounds.music[0].isPlaying) sounds.music[0].play()
+
+      localStorage.musicVolume = volume
+    }
+  )
+  
+  const sfxSlider = slider(
+    'SFX volume', 
+    20, 
+    musicSlider.y + musicSlider.height + 40, 
+    sounds.sfx[0].volume, 
+    volume => {
+      sounds.sfx.forEach(sound => {
+        sound.volume = volume
+        sound.muted = volume < 0.01
+      })
+
+      if (!sounds.sfx[0].isPlaying) sounds.sfx[0].play()
+
+      localStorage.sfxVolume = volume
+    }
+  )
+
 
   wrapper.getChildByName('btn-back').on('click', () => drawTitleScreen(app, state))
 
@@ -25,39 +56,69 @@ export default function drawOptions(app, state) {
 
 }
 
-function slider(name, x, y, sounds) {
+function slider(name, x, y, position, cb) {
   const container = new Container()
   const title = new Text(name.toUpperCase(), { fontFamily : 'Roboto', fontWeight: '400', fontSize: 20, fill: 0x23C1B2 })
   const bar = new Graphics()
 
   container.x = x
   container.y = y
+  container.interactive = true
 
-  bar.y = title.height + 10
-  bar.width = 500
-  bar.height = 20
-  bar.interactive = true
-  
-  const draw = (length) => {
+  const drawBar = pos => {
     bar.clear()
 
     // Background
     bar.beginFill(0x000000)
-    bar.drawRect(0, 0, bar._width, bar._height)
+    bar.drawRect(0, title.height + 10, 500, 20)
     bar.endFill()
 
     // Slider
     bar.beginFill(0x28635C)
-    bar.drawRect(0, 0, length, bar._height)
+    bar.drawRect(0, title.height + 10, 500 * pos, 20)
     bar.endFill()
 
     // Border
     bar.lineStyle(2, 0x23C1B2)
-    bar.drawRect(0, 0, bar._width, bar._height)
-    
+    bar.drawRect(0, title.height + 10, 500, 20)
   }
 
-  draw(500 * sounds[0].volume)
+  drawBar(position)
+
+  container.on('click', e => {
+    let value = (e.data.global.x - container.x) / 500
+    
+    if (value >= 1) value = Math.floor(value)
+    
+    drawBar(value)
+    
+    if (cb) cb(value)
+  })
+
+  container.on('mousemove', (e) => {
+    if (
+      (e.data.global.x < container.x || e.data.global.x > container.x + container.width)  ||
+      (e.data.global.y < container.y + title.height || e.data.global.y > container.y + title.height + bar.height) ||
+      e.data.buttons !== 1
+    ) return
+
+    let value = (e.data.global.x - container.x) / 500
+
+    if (value >= 1) value = Math.floor(value)
+    
+    drawBar(value)
+
+    if (cb) cb(value)
+  })
+
+  container.addChild(title, bar)
+
+  return container
+}
+
+function volumeSlider(name, x, y, sounds) {
+
+  const container = slider(name, x, y, sounds[0].volume)
 
   const changeVolume = (volume) => {
     sounds.forEach(sound => {
@@ -69,27 +130,21 @@ function slider(name, x, y, sounds) {
     if (name == 'sfx volume')   localStorage.sfxVolume   = volume
   }
 
-  bar.on('click', (e) => {
-    draw(e.data.global.x - 20)
+  container.on('click', (e) => {
     changeVolume(1 / 500 * (e.data.global.x - 20))
     if (!sounds[0].isPlaying) sounds[0].play()
-
   })
 
-  bar.on('mousemove', (e) => {
-    
+  container.on('mousemove', (e) => {
     if (
-      (e.data.global.x < container.x || e.data.global.x > container.x + bar._width)  ||
-      (e.data.global.y < container.y + bar.y || e.data.global.y > container.y + bar.y + bar._height) ||
+      (e.data.global.x < container.x || e.data.global.x > container.x + container._width)  ||
+      (e.data.global.y < container.y + container.y || e.data.global.y > container.y + container.y + container._height) ||
       e.data.buttons !== 1
     ) return
 
-    draw(e.data.global.x - 20)
     changeVolume(1 / 500 * (e.data.global.x - 20))
-
   })
 
-  container.addChild(title, bar)
-
   return container
+
 }
