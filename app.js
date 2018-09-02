9c9a0a9a25d9aa0e84a1ea1a956a7f6c4578cf26
Kniffen <knifftech@gmail.com>
2018-09-02 08:21:35 +0200

    1.2.0-beta

--- app.js
+++ app.js
@@ -41807,6 +41807,11 @@ var state = {
 
 state.currentLevel = parseInt(localStorage.currentLevel) || 0;
 state.currentProgress = parseInt(localStorage.currentProgress) || 0;
+state.colorHue = parseInt(localStorage.colorHue) || 0;
+
+var colorMatrix = new PIXI.filters.ColorMatrixFilter();
+app.stage.filters = [colorMatrix];
+app.stage.filters[0].hue(state.colorHue);
 
 _sounds2.default.music.forEach(function (sound) {
   return sound.volume = parseFloat(localStorage.musicVolume) || 0.1;
@@ -42112,8 +42117,10 @@ function drawGame(app, state) {
     name: 'game',
     buttons: ['next', 'reset', 'back', 'quit']
   });
-  var board = new _pixi.Container();
 
+  wrapper.getChildByName('page-title').text = 'LEVEL ' + (state.currentLevel + 1);
+
+  var board = new _pixi.Container();
   board.height = app.view.height * 0.9;
   board.width = board._height / state.grid.length * state.grid[0].length;
   board.x = app.view.width / 2 - board._width / 2;
@@ -42516,72 +42523,96 @@ function drawOptions(app, state) {
     buttons: ['back']
   });
 
-  var musicSlider = slider('music volume', 20, 100, _sounds2.default.music);
-  var sfxSlider = slider('sfx volume', 20, musicSlider.y + musicSlider.height + 40, _sounds2.default.sfx);
+  var musicSlider = slider('Music volume', 20, 100, _sounds2.default.music[0].volume, function (volume) {
+    _sounds2.default.music.forEach(function (sound) {
+      sound.volume = volume;
+      sound.muted = volume < 0.01;
+    });
+
+    if (volume > 0.01 && !_sounds2.default.music[0].isPlaying) _sounds2.default.music[0].play();
+
+    localStorage.musicVolume = volume;
+  });
+
+  var sfxSlider = slider('SFX volume', 20, musicSlider.y + musicSlider.height + 40, _sounds2.default.sfx[0].volume, function (volume) {
+    _sounds2.default.sfx.forEach(function (sound) {
+      sound.volume = volume;
+      sound.muted = volume < 0.01;
+    });
+
+    if (!_sounds2.default.sfx[0].isPlaying) _sounds2.default.sfx[0].play();
+
+    localStorage.sfxVolume = volume;
+  });
+
+  var colorSlider = slider('Color', 20, sfxSlider.y + sfxSlider.height + 40, state.colorHue / 360, function (val) {
+    app.stage.filters[0].hue(val * 360);
+    state.colorHue = val * 360;
+    localStorage.colorHue = val * 360;
+  });
+
+  var musicInfo = new _pixi.Text('Music: Edge of Tomorrow by TeknoAXE\nLicense: Creative Commons Attribution 4.0 International License', { fontFamily: 'Roboto', fontWeight: '400', fontSize: 14, fill: 0x23C1B2 });
+
+  musicInfo.x = 20;
+  musicInfo.y = wrapper.height - 20;
 
   wrapper.getChildByName('btn-back').on('click', function () {
     return (0, _drawTitleScreen2.default)(app, state);
   });
 
-  wrapper.addChild(musicSlider, sfxSlider);
+  wrapper.addChild(musicSlider, sfxSlider, colorSlider, musicInfo);
   app.stage.addChild(wrapper);
 }
 
-function slider(name, x, y, sounds) {
+function slider(name, x, y, position, cb) {
   var container = new _pixi.Container();
   var title = new _pixi.Text(name.toUpperCase(), { fontFamily: 'Roboto', fontWeight: '400', fontSize: 20, fill: 0x23C1B2 });
   var bar = new _pixi.Graphics();
 
   container.x = x;
   container.y = y;
+  container.interactive = true;
 
-  bar.y = title.height + 10;
-  bar.width = 500;
-  bar.height = 20;
-  bar.interactive = true;
-
-  var draw = function draw(length) {
+  var drawBar = function drawBar(pos) {
     bar.clear();
 
     // Background
     bar.beginFill(0x000000);
-    bar.drawRect(0, 0, bar._width, bar._height);
+    bar.drawRect(0, title.height, 500, 20);
     bar.endFill();
 
     // Slider
     bar.beginFill(0x28635C);
-    bar.drawRect(0, 0, length, bar._height);
+    bar.drawRect(0, title.height, 500 * pos, 20);
     bar.endFill();
 
     // Border
     bar.lineStyle(2, 0x23C1B2);
-    bar.drawRect(0, 0, bar._width, bar._height);
+    bar.drawRect(0, title.height, 500, 20);
   };
 
-  draw(500 * sounds[0].volume);
+  drawBar(position);
 
-  var changeVolume = function changeVolume(volume) {
-    sounds.forEach(function (sound) {
-      sound.volume = volume;
-      sound.muted = volume < 0.01;
-    });
+  container.on('click', function (e) {
+    var value = (e.data.global.x - container.x) / 500;
 
-    if (name == 'music volume') localStorage.musicVolume = volume;
-    if (name == 'sfx volume') localStorage.sfxVolume = volume;
-  };
+    if (value >= 1) value = Math.floor(value);
 
-  bar.on('click', function (e) {
-    draw(e.data.global.x - 20);
-    changeVolume(1 / 500 * (e.data.global.x - 20));
-    if (!sounds[0].isPlaying) sounds[0].play();
+    drawBar(value);
+
+    if (cb) cb(value);
   });
 
-  bar.on('mousemove', function (e) {
+  container.on('mousemove', function (e) {
+    if (e.data.global.x < container.x || e.data.global.x > container.x + container.width || e.data.global.y < container.y + title.height || e.data.global.y > container.y + title.height + bar.height || e.data.buttons !== 1) return;
 
-    if (e.data.global.x < container.x || e.data.global.x > container.x + bar._width || e.data.global.y < container.y + bar.y || e.data.global.y > container.y + bar.y + bar._height || e.data.buttons !== 1) return;
+    var value = (e.data.global.x - container.x) / 500;
 
-    draw(e.data.global.x - 20);
-    changeVolume(1 / 500 * (e.data.global.x - 20));
+    if (value >= 1) value = Math.floor(value);
+
+    drawBar(value);
+
+    if (cb) cb(value);
   });
 
   container.addChild(title, bar);
@@ -42691,7 +42722,7 @@ function drawTitleScreen(app, state) {
   });
 
   // Copyright
-  var copyright = new _pixi.Text('v1.1.1-beta\nCOPYRIGHT © 2018 SNURR GAMES', { fontFamily: 'Roboto', fontWeight: '400', fontSize: 10, fill: 0x23C1B2, align: 'right' });
+  var copyright = new _pixi.Text('v1.2.0-beta\nCOPYRIGHT © 2018 SNURR GAMES', { fontFamily: 'Roboto', fontWeight: '400', fontSize: 10, fill: 0x23C1B2, align: 'right' });
 
   copyright.x = app.view.width - copyright.width - 20;
   copyright.y = app.view.height - copyright.height - 20;
@@ -42848,6 +42879,7 @@ function page(_ref) {
   var title = new _pixi.Text(name.toUpperCase(), { fontFamily: 'Roboto', fontWeight: '400', fill: 0x23C1B2 });
 
   container.name = name.toLowerCase();
+  title.name = 'page-title';
   title.x = 20;
   title.y = 20;
 
